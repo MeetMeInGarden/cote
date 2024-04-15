d7f6c4f9cfb50adaa4ffd6eebad0b10868c98ff9
cutemanggom <135785191+MeetMeInGarden@users.noreply.github.com>
2024-04-16 00:23:23 +0900

    문제 023 연결 요소의 개수 구하기
    문제 023 연결 요소의 개수 구하기

---
+++ DfsBfsMain.java
@@ -0,0 +1,92 @@
+package bigtech.dfs_bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+
+// 문제 023 연결 요소의 개수 구하기
+public class DfsBfsMain {
+	// 1. 인접 리스트로 그래프 표현
+	static ArrayList<Integer>[] A;
+	// 2. 방문 배열 초기화
+	static boolean visited[];
+	
+	// 3. 스택에 시작 노트 삽입
+	// TODO
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		// 노드 개수
+		int node = Integer.parseInt(st.nextToken());
+		// 엣지 개수
+		int edge = Integer.parseInt(st.nextToken());
+		
+		A = new ArrayList[node + 1];
+		visited = new boolean[node + 1];
+		
+		for(int i = 1; i < node + 1; i++) {
+			A[i] = new ArrayList<Integer>();
+		}
+		
+		for(int i = 0; i < edge; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			A[s].add(e);
+			A[e].add(s);
+		}
+		
+		int count = 0;
+		for(int i = 1; i < node + 1; i ++) {
+			if(!visited[i]) {
+				count++;
+				DFS(i);
+			}
+		}
+		
+		System.out.println("count : " + count);
+		
+		
+	}
+	
+	private static void DFS(int num) {
+		if(visited[num]) {
+			return;
+		}
+		visited[num] = true;
+		for(int i : A[num]) {
+			if(visited[i] == false) {
+				DFS(i); // 재귀함수
+			}
+		}
+		
+	}
+	// 꺼낸 노드를 탐색 순서에 기입한다.
+	// 꺼낸 노드의 인접 노드를 스택에 삽입하며 이를 방문 배열에 체크한다.
+	// 스택에 삽입할 때 방문 배열과 대조해 이미 방문한 노드는 또 삽입(방문)하지 않게 주의
+}
+
+/*
+ *
+6 5
+1 2
+2 5
+5 1
+3 4
+4 6
+ *
+ *
+ **/
+
+
+
+
+
+
+
+
+
+
+
